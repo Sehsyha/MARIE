7ae6fc89205038b898d6dafc1362dafb0afc4c98
Sehsyha <valentin.stern@zenika.com>
2017-07-19 15:42:11 +0200

    Send MACAddress when arduino is up

--- things/arduino/LightMQTT/LightMQTT.ino
+++ things/arduino/LightMQTT/LightMQTT.ino
@@ -1,18 +1,34 @@
 #include <MQTTClient.h>
 #include <BridgeClient.h>
-#include <Bridge.h>
+#include <Process.h>
 
 BridgeClient net;
 MQTTClient client;
 boolean on;
+String macAddr;
 
 void setup () {
   Bridge.begin();
   client.begin("broker.shiftr.io", net);
   pinMode(13, OUTPUT);
-
+  getMACAddress();
   connect();
   on = false;
+}
+
+void getMACAddress () {
+  Process p;
+  p.runShellCommand("/sbin/ifconfig | grep HWaddr | grep wlan0 | awk '{print $5}'");
+  while (p.running());
+  
+  macAddr = "";
+  
+  while (p.available() > 0) {
+    char c = p.read();
+    if (c > 10) {
+      macAddr.concat(c); 
+    }
+  }
 }
 
 void connect () {
@@ -22,6 +38,12 @@ void connect () {
 
   client.subscribe("/on");
   client.subscribe("/off");
+  client.publish("/register", String("{\"mac-address\":\"" + macAddr + "\","
+                                     "\"type\": \"light\","
+                                     "\"actions\":["
+                                     "{\"name\":\"on\"},"
+                                     "{\"name\":\"off\"}"
+                                     "]}"));
 }
 
 // the loop function runs over and over again forever
@@ -31,13 +53,13 @@ void loop() {
   if (!client.connected()) {
     connect();
   }
-  
+
   if (on) {
-    digitalWrite(13, HIGH); 
+    digitalWrite(13, HIGH);
   } else {
     digitalWrite(13, LOW);
   }
-  
+
 }
 
 void messageReceived(String topic, String payload, char * bytes, unsigned int length) {
